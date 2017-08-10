94d4492ff206eb476ed75c25e191e439ae1ae883
ByakuyaKuchiki <rtsiry@live.fr>
2017-08-10 12:04:19 +0300

    reset
    reset all modules for starting new project

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,19 +2,6 @@ module.exports = function (app) {
 
   //- Home page
   app.get('/', function (req, res) {
-    res.render('introduction.pug');
-  });
-
-  app.get('/home', function (req, res) {
     res.render('home.pug');
   });
-
-  app.get('/form', function (req, res) {
-    res.render('form.pug');
-  });
-
-  app.get('/dash', function (req, res) {
-    res.render('pages/dash.pug');
-  });
-  
 };
--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,37 +1,11 @@
-import { dashboardInstance } from './pages/dashboard.js';
-import { introInstance } from './pages/intro.js';
-import { cardInstance } from '../views/_components/cards/card.js';
-import { registerInstance } from '../views/_components/authentification/register.js';
+
 import { uiControlInstance } from './utilities/ui-control.js';
 
-
-import { dashItemInstance } from './pages/dash.js';
-
-import {menuInstance} from '../views/new_components/menu/menu.js';
-import {showCaseInstance} from '../views/new_components/cards/show-case.js';
 export class Main {
 
   constructor() {
     // dashboardInstance._call();
     // cardInstance._call();
-    uiControlInstance._printing();
-
-    if ($('body').hasClass('intro')) {
-      introInstance._call();
-    }
-
-    if ($('body').hasClass('dashboard')) {
-      dashboardInstance._call();
-      cardInstance._call();
-      registerInstance._call();
-    }
-
-    if($('body').hasClass('dashboard-items')){
-      console.log('test');
-      dashItemInstance._call();
-    }
-
-    menuInstance._switchingMenu();
   }
 }
 
